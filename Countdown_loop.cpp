f756dfaea3509e9d66aac7a20619c97876a5346f
DavCyril <cyriladra024@gmail.com>
2024-08-27 14:04:34 +0000

    Allc++loops

--- Countdown_loop.cpp
+++ Countdown_loop.cpp
@@ -6,11 +6,11 @@ int main(){
     cout<<"This is a program to print out numbers between from 10~1 in descending order using for loops"<<endl;
     cout<<"This is not a drill evacuate the building."<<endl;
 
-    for(int i=10; i>=1; i--){
+    for(int i=1; i<=10; i++){
         cout<<i<<" ";
     }
     cout<<"\n";
-    cout<<"Blast off";
+    
 
 
 
