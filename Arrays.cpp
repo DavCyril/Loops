75f396bca5dbf642e3c1d65087efb0bdf9ad5b00
DavCyril <cyriladra024@gmail.com>
2024-08-27 14:08:08 +0000

    Arrays

---
+++ Arrays.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    string cars[4]=
+            {"volvo","BMW","Toyota","Mazda"};
+            
+            cout<<cars[1];
+
+           
+
+
+
+    return 0;
+}
